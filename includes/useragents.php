f6e0be3f6ec66cffe415d0e14d7470de792678ca
r04r <r04r@minichan.org>
2016-12-05 02:22:19 +0100

    update bot list

--- includes/useragents.php
+++ includes/useragents.php
@@ -7,7 +7,7 @@ function check_user_agent($type = null)
     $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
     if ($type == 'bot') {
         // matches popular bots
-                if (preg_match("/megaindex|linkanalyze|googlebot|adsbot|yandex|feedly|yahooseeker|yahoobot|msnbot|watchmouse|pingdom\.com|feedfetcher-google|ia_archiver|bingbot|mj12bot|ahrefsbot|semrushbot|spbot|check_http/", $user_agent)) {
+                if (preg_match("/megaindex|linkanalyze|googlebot|adsbot|yandex|feedly|yahooseeker|yahoobot|msnbot|watchmouse|pingdom\.com|feedfetcher-google|ia_archiver|bingbot|mj12bot|ahrefsbot|semrushbot|spbot|check_http|quassel/", $user_agent)) {
                     return true;
                         // watchmouse|pingdom\.com are "uptime services"
                 }
